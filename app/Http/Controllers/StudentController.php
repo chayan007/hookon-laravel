fde66192197e5b266a9b9a4839fdbdf88b43f361
Chayan Datta <sonicxxx7@gmail.com>
2018-08-10 10:59:20 +0530

    Admin (8/15)

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -14,31 +14,35 @@ class StudentController extends Controller
     }
     public function edit(Request $request)
     {
-        $student= App\Student::where('id',Auth::user()->id)->firstOrFail();
+        $student= App\Student::where('id',1)->firstOrFail();
         if($request->name != null){
              $student->name=$request->name;
+             $student->save();
         }
         if ($request->dob != null) {
             $student->dob = $request->dob;
+            $student->save();
         }
         if ($request->phone != null) {
             $student->phone = $request->phone;
+            $student->save();
         }
         if ($request->skills != null) {
             $student->skills = $request->skills;
+            $student->save();
         }
         if ($request->photo != null) {
             $file = $request->photo;
             $path = $file->store('public/images/profile');
             $student->photo_url = $path;
+            $student->save();
         }
         if ($request->name != null) {
             $file = $request->cv;
             $path = $file->store('public/documents');
             $student->cv_url = $path;
+            $student->save();
         }
-        $student->save();
-        dd($student);
         redirect('/student/profile');
     }
     public function support(Request $request)
