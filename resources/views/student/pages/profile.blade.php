4fb819875d948d224b8eb81fc591221bb81f6620
ArkaprabhaC <arkaprabha.chatterjee31@gmail.com>
2018-08-30 21:28:51 +0530

    changed UI styling for dashboard for students/recruiters

--- resources/views/student/pages/profile.blade.php
+++ resources/views/student/pages/profile.blade.php
@@ -12,16 +12,16 @@
           </div><!-- /.box-tools -->
         </div><!-- /.box-header -->
         <div class="box-body">
-            <a href="#" class="badge badge-primary" style="padding:10px; margin:10px;">Name :</a>
-            <strong>{{Auth::user()->name}}</strong><br>
-            <a href="#" class="badge badge-primary" style="padding:10px; margin:10px;">Phone :</a>
-            <strong>+91 {{Auth::user()->phone}}</strong><br>
-            <a href="#" class="badge badge-primary" style="padding:10px; margin:10px;">Email :</a>
-            <strong>{{Auth::user()->email}}</strong><br>
-            <a href="#" class="badge badge-primary" style="padding:10px; margin:10px;">Date Of Birth :</a>
-            <strong>{{Auth::user()->dob}}</strong><br>
-            <a href="#" class="badge badge-primary" style="padding:10px; margin:10px;">Skills :</a>
-            <strong>{{Auth::user()->skills}}</strong><br>
+            <h4 href="#" style="font-weight: bold; color: #000; margin:10px; padding-bottom:8px;"><span style="padding-bottom:3px; border-bottom: 2px solid #D95058">Name:</span>&nbsp;&nbsp;
+            {{Auth::user()->name}}<br></h4>
+            <h4 href="#" style="font-weight: bold; color: #000; margin:10px; padding-bottom:8px;"><span style="padding-bottom:3px; border-bottom: 2px solid #D95058">Phone :</span>&nbsp;&nbsp;
+            +91 {{Auth::user()->phone}}<br></h4>
+            <h4 href="#" style="font-weight: bold; color: #000; margin:10px; padding-bottom:8px;"><span style="padding-bottom:3px; border-bottom: 2px solid #D95058">Email :</span>&nbsp;&nbsp;
+            {{Auth::user()->email}}<br></h4>
+            <h4 href="#" style="font-weight: bold; color: #000; margin:10px; padding-bottom:8px;"><span style="padding-bottom:3px; border-bottom: 2px solid #D95058">Date Of Birth :</span>&nbsp;&nbsp;
+            {{Auth::user()->dob}}<br></h4>
+            <h4 href="#" style="font-weight: bold; color: #000; margin:10px; padding-bottom:8px;"><span style="padding-bottom:3px; border-bottom: 2px solid #D95058">Skills :</span>&nbsp;&nbsp;
+            {{Auth::user()->skills}}<br></h4>
         </div><!-- /.box-body -->
       </div><!-- /.box -->
 @stop
