d8494e967362c1c673407fe0dbee975d38e623f2
Chayan Datta <sonicxxx7@gmail.com>
2018-09-12 11:22:55 +0530

    Recruiter Profile Pic Display

--- resources/views/recruiter/includes/panel.blade.php
+++ resources/views/recruiter/includes/panel.blade.php
@@ -6,7 +6,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+          <img src="{{Storage::url(Auth::user()->photo_url)}}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>{{Auth::user()->name}}</p>
